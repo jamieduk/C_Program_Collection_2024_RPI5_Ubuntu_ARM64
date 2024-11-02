63a73e720f1c60767270628d5cfc20a284b8209a
Jay <jamied_uk@hotmail.com>
2024-11-02 12:13:11 +0000

    Add files via upload

---
+++ aio_server_client/ARM/Seperate/client.c
@@ -0,0 +1,71 @@
+// Made By Jay @ J~Net 2024
+//
+//
+// client_gui.c
+// Made By Jay @ J~Net 2024
+#include <gtk/gtk.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int sock;
+char buffer[BUFFER_SIZE];
+
+void connect_to_server(GtkWidget *widget, gpointer data) {
+    struct sockaddr_in serv_addr;
+
+    if ((sock=socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\nSocket creation error\n");
+        return;
+    }
+
+    serv_addr.sin_family=AF_INET;
+    serv_addr.sin_port=htons(PORT);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return;
+    }
+
+    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection Failed\n");
+        return;
+    }
+
+    const char *message=gtk_entry_get_text(GTK_ENTRY(data));
+    send(sock, message, strlen(message), 0);
+
+    int valread=read(sock, buffer, BUFFER_SIZE);
+    buffer[valread]='\0';
+    printf("Echo from server: %s\n", buffer);
+}
+
+int main(int argc, char *argv[]) {
+    gtk_init(&argc, &argv);
+
+    GtkWidget *window=gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    GtkWidget *entry=gtk_entry_new();
+    GtkWidget *button=gtk_button_new_with_label("Send Message");
+
+    g_signal_connect(button, "clicked", G_CALLBACK(connect_to_server), entry);
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    GtkWidget *box=gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+    gtk_box_pack_start(GTK_BOX(box), entry, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(box), button, TRUE, TRUE, 0);
+    gtk_container_add(GTK_CONTAINER(window), box);
+
+    gtk_widget_set_size_request(window, 300, 200);
+    gtk_window_set_title(GTK_WINDOW(window), "Client GUI");
+    gtk_widget_show_all(window);
+
+    gtk_main();
+
+    return 0;
+}
+
---
+++ aio_server_client/x86-64/Seperate/server.c
@@ -0,0 +1,82 @@
+// Made By Jay @ J~Net 2024
+//
+// gcc server.c -o server `pkg-config --cflags --libs gtk+-3.0` -Wall
+//
+// server.c
+// Made By Jay @ J~Net 2024
+#include <gtk/gtk.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h> 
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int server_fd, new_socket;
+char buffer[BUFFER_SIZE];
+
+void start_server(GtkWidget *widget, gpointer data) {
+    int opt=1;
+    struct sockaddr_in address;
+    int addrlen=sizeof(address);
+
+    printf("Starting Server ....\n");
+    if ((server_fd=socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed!");
+        exit(EXIT_FAILURE);
+    }
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family=AF_INET;
+    address.sin_addr.s_addr=INADDR_ANY;
+    address.sin_port=htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    new_socket=accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+    if (new_socket < 0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        int valread=read(new_socket, buffer, BUFFER_SIZE);
+        if (valread <= 0) break;
+        printf("Received: %s\n", buffer);
+        send(new_socket, buffer, valread, 0);
+        memset(buffer, 0, BUFFER_SIZE);
+    }
+    close(new_socket);
+    close(server_fd);
+}
+
+int main(int argc, char *argv[]) {
+    gtk_init(&argc, &argv);
+
+    GtkWidget *window=gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    GtkWidget *button=gtk_button_new_with_label("Start Server");
+
+    g_signal_connect(button, "clicked", G_CALLBACK(start_server), NULL);
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    gtk_container_add(GTK_CONTAINER(window), button);
+    gtk_widget_set_size_request(window, 300, 200);
+    gtk_window_set_title(GTK_WINDOW(window), "Server GUI");
+    gtk_widget_show_all(window);
+    
+    gtk_main();
+
+    return 0;
+}
+
