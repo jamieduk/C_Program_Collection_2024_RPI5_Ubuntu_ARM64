43d43700e6065c71356483cd74ceded2136fa80e
Jay <jamied_uk@hotmail.com>
2024-11-02 16:08:04 +0000

    Add files via upload

--- simple-calc/x86-64/install.sh
+++ simple-calc/x86-64/install.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 # (c) J~Net 2024
 #
-echo "Installing..."
-echo ""
-sudo cp simple-calc /usr/bin/calc
-echo "All Done!"
-echo ""
+function x(){
+sudo chmod +x $1
+
+}
+
+x ./$1
+
+sudo cp ./$1 /usr/bin/
+
+echo "Installed!"
+
